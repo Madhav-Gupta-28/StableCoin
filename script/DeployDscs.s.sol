af08411eff47a8d84c30669d082fc411a9ef4c2d
Madhav-Gupta-28 <guptamadhav2828@gmail.com>
2024-04-16 11:27:00 +0530

    deploy contracts

--- script/DeployDscs.s.sol
+++ script/DeployDscs.s.sol
@@ -4,7 +4,34 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol" ;
+import {DecentralizedStablecoin} from "../src/DecentralizedStablecoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDsc is Script {
 
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns(DecentralizedStablecoin , DSCEngine , HelperConfig) {
+
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
+
+        DecentralizedStablecoin dsc = new DecentralizedStablecoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses,priceFeedAddresses,address(dsc));
+
+        dsc.transferOwnership(address(engine));
+
+        vm.stopBroadcast();
+
+        return (dsc,engine,config);
+
+    }
 }
--- src/DecentralizedStablecoin.sol
+++ src/DecentralizedStablecoin.sol
@@ -1,32 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-
-
-
-// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
-
-// Layout of Contract:
-// version
-// imports
-// interfaces, libraries, contracts
-// errors
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
-
-
 pragma solidity ^0.8.20;
 
 import {ERC20Burnable , ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -52,7 +25,7 @@ contract DecentralizedStablecoin is ERC20Burnable , Ownable{
     error DecentralizedStableCoin__NotZeroAddress() ;
 
 
-    constructor(address addrerss) ERC20("DecentralizedStablecoin","DSC") Ownable(addrerss) {}
+    constructor() ERC20("DecentralizedStablecoin","DSC") Ownable(msg.sender) {}
 
 
     function burn(uint256 _amount) public override onlyOwner{
